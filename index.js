1d2c7e2188413bded4310ce79ed7a9eb650dfb54
Umair Ahmed <ahmedumair83@gmail.com>
2019-07-07 04:21:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 // write your code below!
-
-var array1 = ['Happy Holidays!'];
-
-array1.forEach(function(happyHolidays) {
-  console.log("Happy Holidays!");
-});
+function happyHolidays {
+  say
+}
